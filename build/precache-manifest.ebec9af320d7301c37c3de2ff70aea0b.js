edc1c00bfeb9d2a43f8da2bee4ac528ddf4f1be8
arielcoman <42626517+arielcoman@users.noreply.github.com>
2020-03-02 09:51:25 -0300

    Add files via upload

---
+++ build/precache-manifest.ebec9af320d7301c37c3de2ff70aea0b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba80d7fa82493e5d3554169245854ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56aec304e60df79ef946",
+    "url": "/static/css/2.e20bef6c.chunk.css"
+  },
+  {
+    "revision": "cab7c446045f5ccca023",
+    "url": "/static/css/main.91594a16.chunk.css"
+  },
+  {
+    "revision": "56aec304e60df79ef946",
+    "url": "/static/js/2.5603dc72.chunk.js"
+  },
+  {
+    "revision": "a4d3afa817003ac3902c3660574f36f2",
+    "url": "/static/js/2.5603dc72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cab7c446045f5ccca023",
+    "url": "/static/js/main.04458c2e.chunk.js"
+  },
+  {
+    "revision": "ad8c913e2ae0bb9ac4f6",
+    "url": "/static/js/runtime-main.8e6e9c4f.js"
+  },
+  {
+    "revision": "64984e46a1b49fed9b821fd21fb3ff51",
+    "url": "/static/media/esB.64984e46.jpg"
+  },
+  {
+    "revision": "1d6177029bfd5576762ea22ce38fa1f7",
+    "url": "/static/media/nextL.1d617702.svg"
+  },
+  {
+    "revision": "13947311126c3210d7074e16ddc8b384",
+    "url": "/static/media/nextR.13947311.svg"
+  },
+  {
+    "revision": "3de2eca0554959e3d37cea23d1351c49",
+    "url": "/static/media/search.3de2eca0.svg"
+  }
+]);
